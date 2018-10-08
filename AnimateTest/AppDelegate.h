2863b1e1d9bb991a0c8d8a203fbbb2cf2c7d7082
xukaitian <xukaitian@qiyi.com>
2018-10-08 11:44:19 +0800

    Initial Commit

---
+++ AnimateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimateTest
+//
+//  Created by xukaitiankevin on 2018/10/8.
+//  Copyright © 2018年 xukaitian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimateTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AnimateTest
+//
+//  Created by xukaitiankevin on 2018/10/8.
+//  Copyright © 2018年 xukaitian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
